5c9437318490533ec181e9f4857fafbf7c6a1832
Valerie <146297732+valeosa@users.noreply.github.com>
2026-06-28 00:44:24 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,8 @@ export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
   title: "mySwamp",
   description: "dump your tasks. get your frog.",
-  
+  openGraph: {
+images: ["/og-image.png"],
   },
 };
 
